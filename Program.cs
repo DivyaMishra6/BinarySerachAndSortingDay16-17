1cc6b5dfa73e29ccca30ba906e9cf9f3e7c95f9b
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-31 22:32:40 +0530

    Welcome To the program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using BinarySearchAndSortingDay16_17;
+
+namespace BinarySearchAndSortingDay
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+             Console.WriteLine("!!Welcome to Binary Search Tree and Sorting !!");
+
+            //BubbleSort.DisplayBubble();
+            InsertionSort.DisplayInsertion()
+
+
+        }
+    }
+}
