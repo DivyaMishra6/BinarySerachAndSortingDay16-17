56b25ab8d707f783690c1e36c7e0389ed40b3e44
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-12 00:22:09 +0530

    Using MergeSort

--- Program.cs
+++ Program.cs
@@ -13,11 +13,27 @@ namespace BinarySearchAndSortingDay
             //Anagram.CheckAnagram();
             // PrimeNumber obj = new PrimeNumber();
             //obj.prime();
-    
-             PrimeAnagramPalindrom obj = new PrimeAnagramPalindrom();
-             obj.prime();
+
+            //PrimeAnagramPalindrom obj = new PrimeAnagramPalindrom();
+            // obj.prime();
             //SearchFromFile.Check(@"C:\Users\HP\source\repos\BinarySearchAndSortingDay16&17\BinarySearchAndSortingDay16&17");
 
+            //MergeSort
+            List<string> list = new List<string> { "Durga", "Khali", "Lakshmi", "Sarswati", "Parvati", "Gayatri" };
+            Console.WriteLine("Unsorted List:");
+            foreach (string s in list)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+            List<string> sorted = MergeSortDemo.Sort(list);
+            Console.WriteLine("Sorted List:");
+            foreach (string s in sorted)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
 
         }
     }
