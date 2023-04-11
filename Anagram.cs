d669328d4f5da93d67fe0e37d964e31188f09406
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-11 23:35:15 +0530

    Check Anagram or not

---
+++ Anagram.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchAndSortingDay16_17
+{
+    public class Anagram
+    {
+        public static void CheckAnagram()
+        {
+            string str1 = "heater";
+            string str2 = "rehept";
+
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            String val1 = new string(ch1);
+            String val2 = new string(ch2);
+
+            Console.WriteLine(val1);
+            Console.WriteLine(val2);
+
+            if(val1 == val2)
+            {
+                Console.WriteLine("It is a Anagram");
+            }
+            else
+            {
+                Console.WriteLine("It is a not Anagram");
+            }
+        }
+
+    }
+}
