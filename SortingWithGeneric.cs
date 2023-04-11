91dbede7cd12ea8e5cfc0e4b482a96e92b2e48fb
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-12 00:50:06 +0530

    sorting using generic

---
+++ SortingWithGeneric.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchAndSortingDay16_17
+{
+    public class SortingWithGeneric<T> where T : IComparable<T>
+    {
+        public void DisplayBubble(T[] arr)
+        {
+
+            T temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i].CompareTo(arr[i + 1]) > 0)
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted: ");
+            foreach (T p in arr)
+            {
+                Console.WriteLine(p + " ");
+            }
+        }
+
+        //FOR SEARCHING IN GENERIC
+        public int BinarySearch(T[] arr, T value)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if (arr[mid].Equals(value))
+                {
+                    return mid;
+                }
+                else if (arr[mid].CompareTo(value) < 0)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+
+    }
+}
