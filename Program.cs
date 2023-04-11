ba9f5575d4f332c9badbc0916da9eb633333fb07
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-12 00:48:30 +0530

    Using Generic

--- Program.cs
+++ Program.cs
@@ -19,22 +19,40 @@ namespace BinarySearchAndSortingDay
             //SearchFromFile.Check(@"C:\Users\HP\source\repos\BinarySearchAndSortingDay16&17\BinarySearchAndSortingDay16&17");
 
             //MergeSort
-            List<string> list = new List<string> { "Durga", "Khali", "Lakshmi", "Sarswati", "Parvati", "Gayatri" };
-            Console.WriteLine("Unsorted List:");
-            foreach (string s in list)
-            {
-                Console.Write(s + " ");
-            }
-            Console.WriteLine();
+            //List<string> list = new List<string> { "Durga", "Khali", "Lakshmi", "Sarswati", "Parvati", "Gayatri" };
+            //Console.WriteLine("Unsorted List:");
+            //foreach (string s in list)
+            //{
+            //    Console.Write(s + " ");
+            //}
+            //Console.WriteLine();
 
-            List<string> sorted = MergeSortDemo.Sort(list);
-            Console.WriteLine("Sorted List:");
-            foreach (string s in sorted)
+            //List<string> sorted = MergeSortDemo.Sort(list);
+            //Console.WriteLine("Sorted List:");
+            //foreach (string s in sorted)
+            //{
+            //    Console.Write(s + " ");
+            //}
+            //Console.WriteLine();
+
+            //Generic 
+
+            SortingWithGeneric<int> obj = new SortingWithGeneric<int>();
+            int[] arr = new int[7] { 10, 22, 45, 88, 56, 77, 87 };
+            obj.DisplayBubble(arr);
+            int val = obj.BinarySearch(arr, 56);
+            if (val == -1)
             {
-                Console.Write(s + " ");
+                Console.WriteLine("Value not found in array.");
             }
-            Console.WriteLine();
+            else
+            {
+                Console.WriteLine($"Value found at index {val}.");
+            }
+
 
         }
+
     }
+    
 }
